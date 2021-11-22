408fc6af02855a7547ae696ebb65430d3be092a3
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-11-22 14:42:33 -0500

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-gray-700 flex h-12 items-center">
       <h1 class="text-gray-300 px-4">
-          Bloggy, made by Mehmetali345Dev
+          Bloggy, made by Mehmetali345Dev and uvacoder
       </h1>
   </div>
 </template>
