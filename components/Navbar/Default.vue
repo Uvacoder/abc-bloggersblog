26d60f37c02f217523a78b6e4c299b6e7c368d2e
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-02-04 21:26:22 -0500

    Update Default.vue

--- components/Navbar/Default.vue
+++ components/Navbar/Default.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex h-12 justify-start items-center bg-gray-700">
     <div class="flex items-center">
-      <a href="/" class="text-gray-300 text-3xl font-bold px-4">Bloggy</a>
-      <a target="_blank" href="https://github.com/uvacoder" class="text-gray-300 px-4"> Github </a>
+      <a href="/" class="text-gray-300 text-3xl font-bold px-4">BloggersBlog</a>
+      <a target="_blank" href="https://github.com/uvacoder/bloggersblog" class="text-gray-300 px-4"> Github </a>
       <a href="https://abclinks.vercel.app" class="text-gray-300 px-4"> About </a>
       <a href="/posts" class="text-gray-300 px-4"> All Posts </a>
     </div>
